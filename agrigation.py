e381cdab01862b9ab76ec7721309d94f0c1e010f
purshi <darshandachuu494@gmail.comgit config --user.emaildarshandachuu494@gmai.com>
2023-08-17 03:05:26 -0700

    change4

---
+++ agrigation.py
@@ -0,0 +1,12 @@
+import numpy as np
+a=np.array([[1,3,5],[7,2,1]])
+b=np.array([[6,3,2],[8,5,2]])
+print("sum of array is:",a.sum())
+print("min of array is:",a.min())
+print("max of array is:",a.max())
+print("add of array is:",a+b())
+print("sub of array is:",a-b())
+print("mul of array is:",a*b())
+print("the average of array:",b.mean)
+print("the min of array:",a.min(axis=1))
+print("the max of array:",a.max(axis=1))
